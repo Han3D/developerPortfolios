a0fd7c449824fd56b067631a9ac3a2dabaf2bd62
Han3D <johannes.h@p-42.net>
2025-04-26 17:44:59 +0200

    remove api keys

--- portfolio-v1/src/environments/environment.prod.ts
+++ portfolio-v1/src/environments/environment.prod.ts
@@ -2,16 +2,15 @@
 export const environment = {
   firebase: {
     projectId: 'han3d-portfolio',
-    appId: '1:470924607393:web:289ae13beb8860a3f115e0',
-    databaseURL:
-      'https://han3d-portfolio-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'han3d-portfolio.appspot.com',
+    appId: '',
+    databaseURL: '',
+    storageBucket: '',
     locationId: 'europe-west',
-    apiKey: 'AIzaSyClESf_x9XgBwOu5FEZgXt8GjKxv4xDMh0',
-    authDomain: 'han3d-portfolio.firebaseapp.com',
-    messagingSenderId: '470924607393',
-    measurementId: 'G-LTTLXMBLJ1',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
   },
   production: true,
-  recaptchaSitekey: '6LfnCeQiAAAAANR9smvBrVya69fiqhPIsRoH8Y4P',
+  recaptchaSitekey: '',
 };
