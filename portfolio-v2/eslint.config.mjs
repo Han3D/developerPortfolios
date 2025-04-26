29edeab9bfbabaf890e455916ef82b2ad1e2d33a
Han3D <johannes.h@p-42.net>
2025-04-26 14:59:51 +0200

    added tailwind and eslint and prettier

--- portfolio-v2/eslint.config.mjs
+++ portfolio-v2/eslint.config.mjs
@@ -1,6 +1,36 @@
-// @ts-check
+import prettierPlugin from 'eslint-plugin-prettier'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+	{
+		plugins: {
+			prettier: prettierPlugin,
+		},
+		rules: {
+			'prettier/prettier': [
+				'error',
+				{
+					endOfLine: 'auto',
+				},
+			],
+		},
+	},
+
+	{
+		files: ['**/*.ts', '**/*.vue', '**/*.js'],
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-require-imports': 'off',
+			'@typescript-eslint/unified-signatures': 'off',
+
+			'vue/multi-word-component-names': 'off',
+			'vue/html-self-closing': 'off',
+			'vue/no-multiple-template-root': 'off',
+			'vue/require-default-prop': 'off',
+
+			'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+			'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		},
+	}
 )
--- portfolio-v2/nuxt.config.ts
+++ portfolio-v2/nuxt.config.ts
@@ -1,6 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/scripts']
-})
+  css: ["~/assets/css/main.css"],
+
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/scripts", "motion-v/nuxt"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  compatibilityDate: "2024-11-01",
+});
