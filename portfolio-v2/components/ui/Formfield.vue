0d05c37a1f4a4e9ce43ebd915bcd6b7142319536
Han3D <johannes.h@p-42.net>
2025-05-09 21:04:19 +0200

    refactor: simplify Formfield and Input components by removing unused props and methods

--- portfolio-v2/components/ui/Formfield.vue
+++ portfolio-v2/components/ui/Formfield.vue
@@ -9,11 +9,9 @@
 
 <script setup lang="ts">
 const {
-	label,
 	name,
 	error,
 	touched = false,
-	modelValue,
 } = defineProps<{
 	label: string
 	name: string
@@ -21,25 +19,6 @@ const {
 	touched?: boolean
 	modelValue?: string | number | boolean
 }>()
-
-const emit = defineEmits(['update:modelValue', 'focus', 'blur'])
-
-const isActive = ref(false)
-const hasValue = computed(() => !!modelValue)
-
-function onFocus(e: Event) {
-	console.log('onFocus', e)
-	isActive.value = true
-	emit('focus', e)
-}
-function onBlur(e: Event) {
-	isActive.value = false
-	emit('blur', e)
-}
-function onInput(e: Event) {
-	const target = e.target as HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-	emit('update:modelValue', target.value)
-}
 </script>
 
 <!-- No style block needed, all handled by Tailwind -->
--- portfolio-v2/components/ui/Input.vue
+++ portfolio-v2/components/ui/Input.vue
@@ -1,30 +1,58 @@
 <template>
-	<input
-		v-bind="inputAttrs"
-		:class="['h-12 w-full rounded-lg border px-4 text-neutral-200']"
-		:value="model"
-		v-on="listeners"
-	/>
+	<div class="relative flex w-full flex-col">
+		<motion.input
+			class="h-10 w-full rounded-lg border px-4 text-neutral-200 after:content-['']"
+			:value="model"
+			:name
+			@focus="onFocus"
+			@blur="onBlur"
+		/>
+		<!-- <motion.label
+			:for="name"
+			class="bg-primary-900 absolute top-1/2 left-2 z-10 px-2 text-neutral-400"
+			:style="{
+				transform,
+			}"
+		>
+			{{ name }}
+		</motion.label> -->
+	</div>
 </template>
 
 <script setup lang="ts">
+import { motion } from 'motion-v'
 defineOptions({
 	inheritAttrs: false,
 })
 
 const model = defineModel<string | number>()
 
-const attrs = useAttrs()
+const { name } = defineProps<{
+	name: string
+}>()
+
+// const attrs = useAttrs()
+
+const y = useSpring(-50)
+const transform = useMotionTemplate`translateY(${y}%)`
 
 // Separate listeners (starting with "on") from other attrs
-const listeners = computed(() =>
-	Object.fromEntries(
-		Object.entries(attrs).filter(([key, val]) => key.startsWith('on') && typeof val === 'function')
-	)
-)
-const inputAttrs = computed(() =>
-	Object.fromEntries(Object.entries(attrs).filter(([key, val]) => !key.startsWith('on')))
-)
+// const listeners = computed(() =>
+// 	Object.fromEntries(
+// 		Object.entries(attrs).filter(([key, val]) => key.startsWith('on') && typeof val === 'function')
+// 	)
+// )
+// const inputAttrs = computed(() =>
+// 	Object.fromEntries(Object.entries(attrs).filter(([key, val]) => !key.startsWith('on')))
+// )
+
+function onFocus(e: Event) {
+	y.set(-160)
+}
+
+function onBlur(e: Event) {
+	y.set(-50)
+}
 </script>
 
 <style scoped>
